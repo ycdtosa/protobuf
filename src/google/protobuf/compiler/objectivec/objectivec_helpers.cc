b062de1a49c174438ad697e5aa8a6a5334a8e56b
unknown <ycdtosa@gmail.com>
2019-12-19 18:52:53 +0100

    add guards for radstudio

--- src/google/protobuf/compiler/objectivec/objectivec_helpers.cc
+++ src/google/protobuf/compiler/objectivec/objectivec_helpers.cc
@@ -28,7 +28,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef _MSC_VER
+#if ! defined(_MSC_VER) && ! defined(__CODEGEARC__)
 #include <unistd.h>
 #endif
 #include <climits>
